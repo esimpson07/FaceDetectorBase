88653800b09da606cb63af09269d2a3108623641
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2022-09-13 10:15:19 -0400

    Update facedetector.py
    Attendance checking for congressional app challenge

--- facedetector.py
+++ facedetector.py
@@ -4,16 +4,41 @@ import pickle
 import time
 import cv2
 import os
- 
-cascPathface = os.path.dirname(
- cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
-faceCascade = cv2.CascadeClassifier(cascPathface)
+
+period2 = ["Edward","Brian"]
+attending = [0,0]
+
+unx = 0
+exc = 1
+sus = 2
+exp = 3
+tar = 4
+ins = 5
+
+time = 593 #9:52 am
+enda = 587
+startp2 = 593
+endp2 = 681
+
+faceCascadePath = os.path.dirname(
+ cv2.__file__) + "/data/haarcascade_frontalface_alt.xml"
+faceCascade = cv2.CascadeClassifier(faceCascadePath)
 data = pickle.loads(open(r"C:\Users\edwar\Documents\Coding\Python\facialrecognitioncode\face_enc", "rb").read())
- 
-print("Streaming started")
-video_capture = cv2.VideoCapture(0)
+
+camera = cv2.VideoCapture(1)
+
+def checkNames(val):
+    for i in range(len(period2)):
+        if(val == period2[i]):
+            if(time < startp2):
+                attending[i] = ins
+            elif(time >= startp2 and attending[i] != ins):
+                attending[i] = tar
+            print(attending[i])
+    
 while True:
-    ret, frame = video_capture.read()
+    ret, frame = camera.read()
+    frame = cv2.resize(frame, (300, 200))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(60, 60),flags=cv2.CASCADE_SCALE_IMAGE)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -30,12 +55,14 @@ while True:
                 counts[name] = counts.get(name, 0) + 1
             name = max(counts, key=counts.get)
         names.append(name)
+        checkNames(name)
         for ((x, y, w, h), name) in zip(faces, names):
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
              0.75, (0, 255, 0), 2)
+        print(name)
     cv2.imshow("Frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == 27:
         break
-video_capture.release()
+camera.release()
 cv2.destroyAllWindows()
