17efe2fe2b73d434fa35200f86cc3e4882dad240
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2023-04-09 18:15:17 -0400

    Update facedetector.py

--- facedetector.py
+++ facedetector.py
@@ -4,26 +4,19 @@ import pickle
 import time
 import cv2
 import os
- 
-cascPathface = os.path.dirname(
- cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
-faceCascade = cv2.CascadeClassifier(cascPathface)
+
+faceCascadePath = os.path.dirname(
+ cv2.__file__) + "/data/haarcascade_frontalface_alt.xml"
+faceCascade = cv2.CascadeClassifier(faceCascadePath)
 data = pickle.loads(open(r"C:\Users\edwar\Documents\Coding\Python\facialrecognitioncode\face_enc", "rb").read())
 
-video_capture = cv2.VideoCapture(r"C:\Users\edwar\Pictures\Camera Roll\vid.mp4")
-filepath = r'C:\Users\edwar\Documents\Coding\Python\Videos'
-vidname = filepath + '\output_1.avi'
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(vidname, fourcc, 10, (1920, 1080))
+camera = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = video_capture.read()
+def checkFaces():
+    ret, frame = camera.read()
+    frame = cv2.resize(frame, (300, 200))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(60, 60),flags=cv2.CASCADE_SCALE_IMAGE)
-    detection_result, rejectLevels, levelWeights = faceCascade.detectMultiScale3(frame, scaleFactor=1.0485258, minNeighbors=6,outputRejectLevels = 1)
-    print(rejectLevels)
-    print(levelWeights)
-    print(detection_result)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     encodings = face_recognition.face_encodings(rgb)
     names = []
@@ -42,11 +35,11 @@ while True:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
              0.75, (0, 255, 0), 2)
-    out.write(frame)
+        print(name)
     cv2.imshow("Frame", frame)
     if cv2.waitKey(1) & 0xFF == 27:
-        break
+        camera.release()
+        cv2.destroyAllWindows()
 
-out.release()
-video_capture.release()
-cv2.destroyAllWindows()
+while True:
+    checkFaces()
